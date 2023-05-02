fa2b0388748f2918001b551ccaf0e814e0ae03ea
fuz3Tilt <113349741+fuz3Tilt@users.noreply.github.com>
2023-05-03 02:19:15 +0300

    refactor

--- src/main/java/ru/kradin/blog/utils/FieldErrorsUtil.java
+++ src/main/java/ru/kradin/blog/utils/FieldErrorsUtil.java
@@ -3,15 +3,24 @@ package ru.kradin.blog.utils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FieldErrorsUtil {
 
-    public static Map<String,String> getErrors(BindingResult bindingResult) {
-        Map<String, String> errors = new HashMap<>();
+    public static Map<String, List<String>> getErrors(BindingResult bindingResult) {
+        Map<String, List<String>> errors = new HashMap<>();
         for (FieldError error : bindingResult.getFieldErrors()) {
-            errors.put(error.getField(), error.getDefaultMessage());
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            List<String> fieldErrors = errors.get(fieldName);
+            if (fieldErrors == null) {
+                fieldErrors = new ArrayList<>();
+                errors.put(fieldName, fieldErrors);
+            }
+            fieldErrors.add(errorMessage);
         }
         return errors;
     }
